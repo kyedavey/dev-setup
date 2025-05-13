80aa876cda4435957420c8478500982408fd46c5
Kye Davey <kye.j.davey@gmail.com>
2025-05-13 22:54:55 +1000

    add zellij

---
+++ install/zellij.sh
@@ -0,0 +1,9 @@
+cd /tmp
+wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+tar -xf zellij.tar.gz zellij
+sudo install zellij /usr/local/bin
+rm zellij.tar.gz zellij
+cd -
+
+mkdir -p ~/.config/zellij
+ln -fs ~/code/dev-setup/configs/zellij/config.kdl ~/.config/zellij/config.kdl
