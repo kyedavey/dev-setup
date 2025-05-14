a9eb02d2c922f87a97973afaefd9ac76352b7a83
Kye Davey <kye.j.davey@gmail.com>
2025-05-14 15:55:33 +1000

    remove ghostty

--- install/ghostty.sh
+++
@@ -1,4 +0,0 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
-
-mkdir -p ~/.config/ghostty
-ln -fs ~/code/dev-setup/configs/ghostty/config ~/.config/ghostty/config
