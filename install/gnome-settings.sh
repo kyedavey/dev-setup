58bea3016a4d170a58bc7c434669a9e9512cab53
Kye Davey <kye.j.davey@gmail.com>
2025-05-14 12:09:04 +1000

    set reduced animations

--- install/gnome-settings.sh
+++ install/gnome-settings.sh
@@ -3,3 +3,6 @@ gsettings set org.gnome.mutter center-new-windows true
 
 # Set Cascadia Mono as the default monospace font
 gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Nerd Font 10'
+
+# Reduce animations
+gsettings set org.gnome.desktop.interface enable-animations false
