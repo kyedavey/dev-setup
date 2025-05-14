1f9a7d0ab36db6273fc60e126aad4e9b772c63e5
Kye Davey <kye.j.davey@gmail.com>
2025-05-14 15:54:23 +1000

    change ghostty installer

--- install/ghostty.sh
+++ install/ghostty.sh
@@ -1,19 +1,4 @@
-GHOSTTY_DEB_URL=$(
-   curl -s https://api.github.com/repos/mkasberg/ghostty-ubuntu/releases/latest | \
-   grep -oP "https://github.com/mkasberg/ghostty-ubuntu/releases/download/[^\s/]+/ghostty_[^\s/_]+_${SUFFIX}.deb"
-)
-if [[ -z "$GHOSTTY_DEB_URL" ]]; then
-  echo "Error: Failed to retrieve the .deb package URL from GitHub."
-  exit 1
-fi
-GHOSTTY_DEB_FILE=$(basename "$GHOSTTY_DEB_URL")
-
-echo "Downloading ${GHOSTTY_DEB_FILE}..."
-curl -LO "$GHOSTTY_DEB_URL"
-
-echo "Installing ${GHOSTTY_DEB_FILE}..."
-sudo dpkg -i "$GHOSTTY_DEB_FILE"
-rm "$GHOSTTY_DEB_FILE"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
 
 mkdir -p ~/.config/ghostty
 ln -fs ~/code/dev-setup/configs/ghostty/config ~/.config/ghostty/config
