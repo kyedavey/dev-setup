8219af98826948da4292a75c060caadd3ac490dc
Kye Davey <kye.j.davey@gmail.com>
2025-06-12 20:10:35 +1000

    add wallpaper

--- install/desktop/gnome-theme.sh
+++ install/desktop/gnome-theme.sh
@@ -5,7 +5,7 @@ gsettings set org.gnome.desktop.interface gtk-theme "Yaru-bark-dark"
 gsettings set org.gnome.desktop.interface icon-theme "Yaru-bark"
 gsettings set org.gnome.desktop.interface accent-color "bark" 2>/dev/null || true
 
-BACKGROUND_ORG_PATH="$HOME/code/dev-setup/theme/background.jpg"
+BACKGROUND_ORG_PATH="/tmp/dev-setup/assets/wallpaper.jpg"
 BACKGROUND_DEST_DIR="$HOME/.local/share/backgrounds"
 BACKGROUND_DEST_PATH="$BACKGROUND_DEST_DIR/background.jpg"
 
