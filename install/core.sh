8c74a1d8fa898043e8db3d189d91873adfc8cbc3
Kye Davey <kye.j.davey@gmail.com>
2025-06-12 19:17:14 +1000

    fix paths

--- install/core.sh
+++ install/core.sh
@@ -1,3 +1,3 @@
 ~/.dotfiles/dotfiles.sh -f install bash
 
-for installer in ~/tmp/dev-setup/install/core/*.sh; do source $installer; done
+for installer in /tmp/dev-setup/install/core/*.sh; do source $installer; done
--- install/desktop.sh
+++ install/desktop.sh
@@ -2,8 +2,8 @@
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
 
-for installer in ~/tmp/dev-setup/install/core/*.sh; do source $installer; done
-for installer in ~/tmp/dev-setup/install/desktop/*.sh; do source $installer; done
+for installer in /tmp/dev-setup/install/core/*.sh; do source $installer; done
+for installer in /tmp/dev-setup/install/desktop/*.sh; do source $installer; done
 
 # Set gnome bar apps
 gsettings set org.gnome.shell favorite-apps "['Alacritty.desktop', 'google-chrome.desktop',  'code.desktop']"
